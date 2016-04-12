c66896d7093775298f601403adc9f72bad01b6a6
Sergio Diaz <sdiazn19@gmail.com>
2016-04-12 17:44:28 -0500

    Clase Cuadro

--- Proyecto_Final/Flood/src/Flood/Cuadro.java
+++ Proyecto_Final/Flood/src/Flood/Cuadro.java
@@ -1,19 +1,37 @@
 
 package Flood;
 
+import java.awt.Image;
+
 /**
  *
- * @author SDN
+ * @author Left & Right
  */
-public class Cuadro {
+public class Cuadro extends Base {
     
     private int iIndex;
     private boolean isActive;
     
+
+    public Cuadro(int iX, int iY, Image imaImagen) {
+        super(iX, iY, imaImagen);
+    }
+    
+    public int getIndex(){
+        
+        return iIndex;
+    }
+    
+    public void setIndex(int iIndex){
+        
+        this.iIndex = iIndex;
+    }
     
     
+    public boolean isActive(){
+        
+        return isActive;
+    }
     
-    
-    
-    
+ 
 }
